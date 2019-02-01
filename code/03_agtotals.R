0bca173704942bf45130b96531a06b7f84ebdcb3
Kristen Dybala <kdybala@gmail.com>
2019-02-01 12:03:25 -0800

    add code stub for generating ag totals

---
+++ code/03_agtotals.R
@@ -0,0 +1,14 @@
+# README------------
+# Estimate proportion of state-wide annual crop totals that fall within
+# CVJV primary focus area and each basin using TNC-generated spatial layer
+# representing "consistent" classification of general crop classes
+
+# PACKAGES
+library(tidyverse)
+
+# INPUT DATA
+agstats <- 'data/annual_NASS_totals_statewide.csv'
+# TNC raster
+
+# OUTPUT DATA
+csv
