1df9f3ec20b6a9e64a0038facc49db5029d50721
Kristen Dybala <kdybala@gmail.com>
2019-02-01 17:57:46 -0800

    refine nass function to better match CVJV data

--- code/02_agstats.R
+++ code/02_agstats.R
@@ -16,48 +16,50 @@ library(tidyverse)
 source(here::here('functions/extract_nass.R'))
 
 # OUTPUT
+rawdat <- 'data/annual_NASS_raw_statewide.csv'
 agstats <- 'data/annual_NASS_totals_statewide.csv'
 
 
-# NASS QUICKSTATS-------------
+# EXTRACT QUICKSTATS-------------
 ## extract annual totals of rice, corn, other field crops (beans, barley, oats,
 ##  safflower, sorghum, sugarbeets, wheat),  row crops (vegetable totals),
 ##  and alfalfa
 
-dat <- do.call(bind_rows, lapply(c(2007:2017), extract_nass))
+dat <- do.call(bind_rows, lapply(c(2007:2017), extract_nass, cvjv.match = FALSE))
+write_csv(dat, here::here(rawdat))
 
-## add irrigated pasture data (from 2013 census)
-dat2 <- bind_rows(dat, 
-                  nass_data(source_desc = 'CENSUS', 
-                            commodity_desc = 'PASTURELAND',
-                            domain_desc = 'TOTAL',
-                            statisticcat_desc = 'AREA',
-                            unit_desc = 'ACRES',
-                            agg_level_desc = 'STATE',
-                            state_name = 'CALIFORNIA', 
-                            token = Sys.getenv('NASS_TOKEN'))) 
-
-## Note: alfalfa acres planted may not reflect total acreage since it's
-##  a perennial
-
-## group together the other field and row crops, and summarize by year
-sdat <- dat2 %>% 
+# SUMMARIZE------------------
+## group together the other field and row crops, and summarize by year #MINT?
+sdat <- read_csv(here::here(rawdat)) %>% 
   select(commodity_desc, 
          year, 
          acres = Value) %>%
+  # match some values that were different in original NASS data used for CVJV
+  mutate(acres = case_when(commodity_desc == 'VEGETABLE TOTALS' & year == 2014 & acres == 737300 ~ 750000, 
+                           commodity_desc == 'COTTON' & year == 2010 & acres == 306000 ~ 303000,
+                           commodity_desc == 'COTTON' & year == 2007 & acres == 455000 ~ 451000,
+                           TRUE ~ acres)) %>%
+  # group into crop classes
   mutate(crop_class = case_when(commodity_desc == 'CORN' ~ 'corn',
                                 commodity_desc == 'RICE' ~ 'rice',
-                                commodity_desc == 'HAY & HAYLAGE' ~ 'alfalfa',
-                                commodity_desc == 'PASTURELAND' ~ 'pasture',
                                 commodity_desc == 'VEGETABLE TOTALS' ~ 'rowcrop',
                                 commodity_desc %in% c('BARLEY', 'OATS', 'WHEAT') ~ 'grains',
-                                commodity_desc %in% c('BEANS', 'COTTON', 'SAFFLOWER', 'SORGHUM', 'SUGARBEETS', 'SUNFLOWER') ~ 'fieldcrop',
+                                commodity_desc %in% c('BEANS', 'COTTON', 'MINT', 
+                                                      'SAFFLOWER', 'SORGHUM', 
+                                                      'SUGARBEETS', 'SUNFLOWER') ~ 'fieldcrop',
                                 TRUE ~ NA_character_),
          year = as.numeric(year)) %>%
+  filter(!is.na(acres)) %>%
   group_by(crop_class, year) %>%
   summarize(acres = sum(acres))
 
+## check for missing values
+sdat %>% spread(key = 'year', value = 'acres')
+
 ## plot (remember these are state-wide totals)
+dat %>% ggplot(aes(as.numeric(year), Value/1000, color = as.factor(commodity_desc))) + 
+  geom_line() + geom_point() + scale_color_viridis_d() + theme_classic() +
+  scale_x_continuous(breaks = seq(2007, 2017, 1))
 sdat %>% ggplot(aes(year, acres/1000, color = crop_class)) + 
   geom_line() + geom_point() + scale_color_viridis_d() + theme_classic() +
   scale_x_continuous(breaks = seq(2007, 2017, 1))
--- functions/extract_nass.R
+++ functions/extract_nass.R
@@ -1,4 +1,4 @@
-extract_nass = function(t) {
+extract_nass = function(t, cvjv.match = TRUE) {
   res <- bind_rows(
     ## field crops
     nass_data(source_desc = 'SURVEY', 
@@ -24,16 +24,16 @@ extract_nass = function(t) {
               year = as.character(t), 
               reference_period_desc = 'YEAR',
               token = Sys.getenv('NASS_TOKEN' )),
-    ## and alfalfa
-    nass_data(source_desc = 'SURVEY', 
-              sector_desc = 'CROPS', 
-              commodity_desc =  'HAY & HAYLAGE', 
-              statisticcat_desc = 'AREA PLANTED',
-              agg_level_desc = 'STATE',
-              state_name = 'CALIFORNIA', 
-              year = as.character(t), 
-              reference_period_desc = 'YEAR',
-              token = Sys.getenv('NASS_TOKEN' )),
+    # ## and alfalfa
+    # nass_data(source_desc = 'SURVEY', 
+    #           sector_desc = 'CROPS', 
+    #           commodity_desc =  'HAY & HAYLAGE', 
+    #           statisticcat_desc = 'AREA PLANTED',
+    #           agg_level_desc = 'STATE',
+    #           state_name = 'CALIFORNIA', 
+    #           year = as.character(t), 
+    #           reference_period_desc = 'YEAR',
+    #           token = Sys.getenv('NASS_TOKEN' )),
     ## and vegetables
     if (t < 2016) {
       nass_data(source_desc = 'SURVEY', 
@@ -59,6 +59,34 @@ extract_nass = function(t) {
                 year = as.character(t), 
                 reference_period_desc = 'YEAR',
                 token = Sys.getenv('NASS_TOKEN' )) 
-    }
+    },
+    ## and mint (included in field crops used by CVJV, keep for comparability)
+    if (cvjv.match == TRUE & t > 2007) {
+      nass_data(source_desc = 'SURVEY',
+                sector_desc = 'CROPS',
+                commodity_desc = 'MINT',
+                class_desc = 'PEPPERMINT',
+                prodn_practice_desc = 'ALL PRODUCTION PRACTICES',
+                statisticcat_desc = 'AREA HARVESTED',
+                agg_level_desc = 'STATE',
+                state_name = 'CALIFORNIA',
+                year = as.character(t),
+                reference_period_desc = 'YEAR',
+                token = Sys.getenv('NASS_TOKEN'))
+    },
+    ## and double-counting some vegetables by mistake, keep for comparability to CVJV
+    if (cvjv.match == TRUE & t < 2016) {
+      nass_data(source_desc = 'SURVEY', 
+                sector_desc = 'CROPS', 
+                group_desc = 'VEGETABLES', 
+                commodity_desc = 'VEGETABLE TOTALS',
+                class_desc = '10 MAJOR, VARIES BY SEASON',
+                statisticcat_desc = 'AREA PLANTED',
+                agg_level_desc = 'STATE',
+                state_name = 'CALIFORNIA', 
+                year = as.character(t), 
+                reference_period_desc = 'YEAR',
+                token = Sys.getenv('NASS_TOKEN' ))
+    } 
   )
 }
