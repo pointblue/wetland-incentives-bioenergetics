067a9dc0cfb3f4f7777db95f0abe93ba098593ae
Kristen Dybala <kdybala@gmail.com>
2019-02-01 17:59:02 -0800

    estimate basin crop class totals by year

--- code/03_agtotals.R
+++ code/03_agtotals.R
@@ -8,7 +8,50 @@ library(tidyverse)
 
 # INPUT DATA
 agstats <- 'data/annual_NASS_totals_statewide.csv'
-# TNC raster
+agdist <- 'data/ag_distribution_basins.csv'
 
 # OUTPUT DATA
-csv
+results <- 'output/planted_crops_mean_2013-17.csv'
+
+
+# ANNUAL BASIN TOTALS------------
+# allocate annual crop totals to basins (and therefore the CVJV primary focus area)
+# based on average spatial distribution of crops in CA (same spatial distribution
+# assumed for CVJV non-breeding shorebirds paper)
+
+dist <- read_csv(here::here(agdist)) %>%
+  filter(TREND_CATE %in% c('Grass/Pasture', 'Field Crop', 'Row Crop', 'Alfalfa',
+                           'Grains', 'Corn', 'Rice')) %>%
+  rename(basin = Primary_Ba,
+         crop_class = TREND_CATE,
+         prop = perc) %>% # it's actually the proportion
+  select(ID, basin, crop_class, prop) %>%
+  mutate(crop_class = tolower(crop_class),
+         crop_class = gsub(' ', '', crop_class),
+         crop_class = recode(crop_class, 'grass/pasture' = 'pasture')) 
+
+dat <- read_csv(here::here(agstats)) %>%
+  left_join(dist, by = 'crop_class') %>%
+  mutate(ha = acres * prop / 2.47105) %>%
+  select(crop_class, ID, basin, year, ha) %>%
+  filter(basin != 'Suisun') %>%
+  filter(!(crop_class == 'corn' & basin %in% c('San Joaquin', 'Tulare'))) %>%
+  mutate(crop_class = recode(crop_class,
+                             fieldcrop = 'other',
+                             rowcrop = 'other',
+                             grains = 'other'))
+
+# summarize for 2007-2014 (compare to CVJV totals) --> very close ("other" is lower now)
+dat %>% 
+  filter(year <= 2014) %>%
+  group_by(crop_class, year) %>%
+  summarize(ha = sum(ha)) %>%
+  group_by(crop_class) %>%
+  summarize(ha = mean(ha))
+
+# summarize total for each crop class in each year
+sdat <- dat %>% 
+  group_by(crop_class, year) %>%
+  summarize(ha = sum(ha)) 
+
+write_csv(sdat, here::here(results))
