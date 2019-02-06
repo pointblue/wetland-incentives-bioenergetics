922a93e64dbc94be2de661e1b04ac97b5c17dde0
Kristen Dybala <kdybala@gmail.com>
2019-02-06 15:25:42 -0800

    add stub

---
+++ code/3b_update_flooding_curves.R
@@ -0,0 +1,7 @@
+# README---------------
+# Compile time series of habitat available and accessible from crops and 
+# wetlands not enrolled in incentive programs. Because areas of these programs
+# are relatively small, ignore them?
+#
+# STEP 2: update flooding curves
+#
