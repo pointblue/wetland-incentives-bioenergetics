fbbf8c4daed31c96ad9263e0ef26bdc56322ded2
Kristen Dybala <kdybala@gmail.com>
2019-02-12 17:21:04 -0800

    add code for fitting new flood curves

--- code/3b_update_flooding_curves.R
+++ code/3b_update_flooding_curves.R
@@ -5,3 +5,165 @@
 #
 # STEP 2: update flooding curves
 #
+# PACKAGES
+library(tidyverse)
+library(gamm4)
+
+# FUNCTIONS
+# custom function to fit GAMMs to water tracker data
+source(here::here('functions/fit_flooding_curves_gamm4.R'))
+
+
+# INPUTS
+tracker <- 'data/stats_basin_ag.csv' #data from water tracker (via Nathan)
+
+# OUTPUTS
+floodcurves <- 'output/open_water_annual.csv'
+floodcurves_overall <- 'output/open_water_overall.csv'
+
+# RAW DATA-------------
+# from water tracker automated processing; cloud-filled!
+
+dat <- read_csv(here::here(tracker), col_types = cols()) %>%
+  # drop extra column
+  select(-X12) %>% 
+  # drop Suisun basin
+  filter(BasinName != 'Suisun') %>%
+  # drop crop classes we aren't using:
+  filter(ClassName %in% c('Corn', 'Rice', 'Field Crop', 'Grains', 'Row Crop', 'Wetland')) %>%
+  # group row, field, and grains into "Other"
+  mutate(ClassName = recode(ClassName,
+                            'Field Crop' = 'Other',
+                            'Row Crop' = 'Other',
+                            'Grains' = 'Other')) %>%
+  mutate_at(vars(DataSource:Mosaic), as.factor) %>% 
+  mutate_at(vars(MosaicDateStart:MosaicDateEnd), as.Date, format = '%m/%d/%Y') 
+  
+
+# OPEN WATER BY CROP CLASS----------
+# summarize open water data by crop class over all basins (except Suisun)
+
+sdat <- dat %>%
+  # first back-calculate total area from percent observed and observed area
+  mutate(TotalArea = ObservedArea / (PercentObserved/100)) %>%
+  # drop non-existent Tulare Rice:
+  filter(Name != 'Tulare Rice') %>%
+  # drop unsuitable corn in Tulare and San Joaquin basins:
+  filter(!(Name %in% c('Tulare Corn', 'San Joaquin Corn'))) %>%
+  group_by(ClassName, Mosaic, MosaicDateStart, MosaicDateEnd) %>%
+  # summarize and conver to number of pixels (30x30m)
+  summarize(TotalArea = sum(TotalArea) / 900,
+            ObservedArea = sum(ObservedArea) / 900,
+            ObservedAreaWater = sum(ObservedAreaWater) / 900,
+            PropObserved = ObservedArea/TotalArea,
+            PropWater = ObservedAreaWater/ObservedArea) %>%
+  ungroup() %>%
+  # # filter dates to remove duplicates
+  # mutate(monthpart = case_when(as.numeric(format(MosaicDateStart, '%d')) <= 15 & 
+  #                                as.numeric(format(MosaicDateEnd, '%d')) <= 15 ~ 'A',
+  #                              as.numeric(format(MosaicDateStart, '%d')) >= 16 &
+  #                                as.numeric(format(MosaicDateEnd, '%d')) >= 16 ~ 'B',
+  #                              MosaicDateStart %in% c('2015-10-08', '2015-10-31') ~ 'A',
+  #                              MosaicDateStart %in% c('2014-12-15', '2015-11-16') ~ 'B',
+  #                              TRUE ~ NA_character_)) %>%
+  # filter(!is.na(monthpart)) %>%
+  # add mid-point of mosaic date range:
+  mutate(MosaicDateMid = MosaicDateStart + (MosaicDateEnd - MosaicDateStart)/2,
+         month = as.numeric(format(MosaicDateMid, '%m')),
+         yday = as.numeric(format(MosaicDateMid, '%j')),
+         yday = case_when(month <= 6 ~ yday + 184,
+                          month >= 7 ~ yday - 181),
+         bioyear = as.numeric(format(MosaicDateMid, '%Y')),
+         bioyear = case_when(month <=6 ~ bioyear - 1,
+                             TRUE ~ bioyear)) %>%
+  filter(bioyear >= 2013 & bioyear <= 2017) %>%
+  mutate(label = recode(bioyear, 
+                        '2013' = '2013-14',
+                        '2014' = '2014-15',
+                        '2015' = '2015-16',
+                        '2016' = '2016-17',
+                        '2017' = '2017-18'))
+
+ggplot(sdat %>% filter(yday < 330), 
+       aes(yday, PropWater, color = as.factor(label))) + 
+  geom_point() + facet_wrap(~ClassName) 
+
+
+# FIT GAMMS-------------
+# same model calls as in CVJV work, but eliminate some duplicated scenes from
+#  rolling mosaics
+
+tmp <- sdat %>% filter(ClassName == 'Wetland' & yday <= 319) %>% 
+  arrange(MosaicDateStart)
+for (i in c(1:(nrow(tmp) - 1))) {
+  if (tmp$MosaicDateEnd[i] >= tmp$MosaicDateStart[i+1]) tmp = tmp[-i,]
+}  
+wetlands <- fit_gamm4(tmp, 
+                      dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                      nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater', 
+                      minprop = 0.4, k = 20, by = 'label') 
+wetlands_overall <- fit_gamm4(tmp, 
+                              dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                              nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater', 
+                              minprop = 0.4, k = 20) 
+
+
+tmp <- sdat %>% filter(ClassName == 'Rice' & yday <= 319) %>% 
+  arrange(MosaicDateStart)
+for (i in c(1:(nrow(tmp) - 1))) {
+  if (tmp$MosaicDateEnd[i] >= tmp$MosaicDateStart[i+1]) tmp = tmp[-i,]
+} 
+rice <- fit_gamm4(tmp,
+                  dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea',
+                  nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                  minprop = 0.4, k = 20, by = 'label') 
+rice_overall = fit_gamm4(tmp,
+                         dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea',
+                         nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                         minprop = 0.4, k = 20) 
+
+tmp <- sdat %>% filter(ClassName == 'Corn' & yday <= 319) %>% 
+  arrange(MosaicDateStart)
+for (i in c(1:(nrow(tmp) - 1))) {
+  if (tmp$MosaicDateEnd[i] >= tmp$MosaicDateStart[i+1]) tmp = tmp[-i,]
+} 
+corn = fit_gamm4(tmp, 
+                 dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                 nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                 minprop = 0.4, k = 20, by = 'label')
+
+corn_overall = fit_gamm4(tmp, 
+                         dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                         nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                         minprop = 0.4, k = 20) 
+
+tmp <- sdat %>% filter(ClassName == 'Other' & yday <= 319) %>% 
+  arrange(MosaicDateStart)
+for (i in c(1:(nrow(tmp) - 1))) {
+  if (tmp$MosaicDateEnd[i] >= tmp$MosaicDateStart[i+1]) tmp = tmp[-i,]
+} 
+other = fit_gamm4(tmp, 
+                  dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                  nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                  minprop = 0.4, k = 20, by = 'label') 
+other_overall = fit_gamm4(tmp, 
+                          dayofyear = 'yday', year = 'bioyear', ntotal = 'TotalArea', 
+                          nsampled = 'ObservedArea', nflooded = 'ObservedAreaWater',
+                          minprop = 0.4) 
+
+## combine predicted values:
+pred <- wetlands$pred %>% mutate(landcover = 'wetlands') %>%
+  bind_rows(rice$pred %>% mutate(landcover = 'rice')) %>% 
+  bind_rows(corn$pred %>% mutate(landcover = 'corn')) %>%
+  bind_rows(other$pred %>% mutate(landcover = 'other')) %>% 
+  mutate(landcover = as.factor(landcover)) 
+
+pred_overall <- wetlands_overall$pred %>% mutate(landcover = 'wetlands') %>%
+  bind_rows(rice_overall$pred %>% mutate(landcover = 'rice')) %>% 
+  bind_rows(corn_overall$pred %>% mutate(landcover = 'corn')) %>%
+  bind_rows(other_overall$pred %>% mutate(landcover = 'other')) %>% 
+  mutate(landcover = as.factor(landcover)) 
+
+
+write_csv(pred, here::here(floodcurves))
+write_csv(pred_overall, here::here(floodcurves_overall))
---
+++ functions/fit_flooding_curves_gamm4.R
@@ -0,0 +1,153 @@
+fit_gamm4 = function(df, dayofyear = 'yday', nsampled = 'nsampled', 
+                     nflooded = 'nflooded', ntotal = 'ntotal', year = 'year',
+                     minprop = NULL, plot = TRUE, k = 10, by = NULL) {
+  require(gamm4)
+  
+  # set up variables:
+  df <- df %>%
+    rename(yday = dayofyear, ntotal = ntotal, nsampled = nsampled, 
+           nflooded = nflooded, year = year) %>%
+    mutate(zday = (yday - 150) / 100,
+           nflooded = as.integer(nflooded),
+           nsampled = as.integer(nsampled)) 
+
+  # sampledat$precip.2wk = sampledat$precip.2wk/10 #convert tenths of mm to mm
+  
+  # set minimum proportion sampled for interval to be included in data set
+  if (!is.null(minprop)) {
+    df <- df %>% 
+      mutate(weights = nsampled / ntotal) %>%
+      filter(weights >= minprop)
+  } else {
+    df <- df %>%
+      mutate(weights = NULL)
+  }
+
+  # unique sample id
+  df$id <- c(1:nrow(df)) 
+  
+  print(paste('Observations:', nrow(df)))
+  
+  if (is.null(by)) {
+    res <- gamm4(cbind(nflooded, nsampled - nflooded) ~ s(zday, k = k), 
+                 random = ~(1 | year) + (1 | id), 
+                 family = binomial, data = df, weights = weights)
+    
+    newdat <- data.frame(yday = seq(1, 319, 1), 
+                         prop.flooded = 0, 
+                         nflooded = 0, 
+                         nsampled = 0, 
+                         prop.sampled = 0)
+
+  } else if (!is.null(by)) {
+    df <- df %>%
+      rename(group = by) %>%
+      mutate(group = as.factor(group))
+    
+    res <- gamm4(cbind(nflooded, nsampled - nflooded) ~ s(zday, k = k, by = group), 
+                 random = ~(1 | id), 
+                 family = binomial, data = df, weights = weights)
+    
+    newdat <- expand.grid(yday = seq(1, 319, 1), 
+                          group = levels(df$group)) %>%
+      mutate(prop.flooded = 0, nflooded = 0, nsampled = 0, prop.sampled = 0)
+  }
+  
+  # predict proportion open water by day of year with CI
+  newdat$zday = (newdat$yday - 150) / 100
+  newdat$prop.flooded <- predict(res$gam, newdat, type = 'response')
+  pred <- as.data.frame(predict(res$gam, newdat, type = 'link', se.fit = T))
+  newdat$lcl <- plogis(pred$fit - 2 * pred$se.fit)
+  newdat$ucl <- plogis(pred$fit + 2 * pred$se.fit)
+  newdat$nflooded <- NULL
+  newdat$nsampled <- NULL
+  newdat$prop.sampled <- NULL
+  newdat$zday <- NULL
+
+  res <- list(mod = res, sampledat = df, pred = newdat)
+  
+  if (plot == TRUE) {
+    require(ggplot2)
+    p <- ggplot(newdat, aes(x = yday, y = prop.flooded)) +
+      scale_x_continuous(limits = c(1, 319), 
+                         expand = c(0, 0), 
+                         breaks = c(1, 32, 62, 93, 124, 154, 185, 216, 244, 
+                                    275, 305), 
+                         labels = c('Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec', 
+                                    'Jan', 'Feb', 'Mar', 'Apr', 'May')) +
+      scale_y_continuous(limits = c(0, 1), 
+                         expand = c(0, 0),
+                         breaks = seq(0, 1, 0.2)) +
+      xlab(NULL) + ylab('Proportion open water') +
+      scale_size(range = c(1, 4), guide = 'none') +
+      theme_classic() + 
+      theme(legend.position = c(1, 1), legend.justification = c(1, 1))
+    
+    if (!is.null(by)) {
+      p <- p + 
+        # geom_ribbon(aes(ymin = lcl, ymax = ucl, fill = group), alpha = 0.3) +
+        geom_line(aes(color = group), size = 1) +
+        geom_point(data = df, 
+                   aes(y = nflooded/nsampled, size = nsampled/ntotal, 
+                       color = group), shape = 21) 
+    } else {
+      p <- p + 
+        geom_ribbon(aes(ymin = lcl, ymax = ucl), alpha = 0.5, fill = 'gray50') + 
+        geom_line() +
+        geom_point(data = df, shape = 21,
+                   aes(y = nflooded/nsampled, size = nsampled/ntotal))
+    }
+    
+    print(p)      
+  }
+  
+  return(res)
+    
+  # } 
+  # else if (precip == TRUE) {
+  #   res <- gamm4(cbind(n.flooded, n.sampled - n.flooded) ~ s(zday, k = k) + precip.2wk, 
+  #                random = ~(1 | wateryear) + (1 | id), 
+  #                family = binomial, data = sub, weights = weight)
+  #   
+  #   ## predicted proportion open water with zero precip in previous 2wks
+  #   newdat <- data.frame(yday = seq(1, 319, 1), 
+  #                        precip.2wk = 0, 
+  #                        prop.flooded = 0, 
+  #                        n.flooded = 0, 
+  #                        n.sampled = 0, 
+  #                        prop.sampled = 0)
+  #   newdat$zday = (newdat$yday - 150) / 100
+  #   newdat$prop.flooded <- predict(res$gam, newdat, type = 'response')
+  #   pred <- as.data.frame(predict(res$gam, newdat, type = 'link', se.fit = T))
+  #   newdat$lcl <- plogis(pred$fit - 2 * pred$se.fit)
+  #   newdat$ucl <- plogis(pred$fit + 2 * pred$se.fit)
+  #   
+  #   ## predicted proportion open water with 10mm in previous 2 wks -- range observed is 0-12.27
+  #   newdat2 <- data.frame(yday = seq(1, 319, 1), 
+  #                         precip.2wk = 10, 
+  #                         prop.flooded = 0, 
+  #                         n.flooded = 0, 
+  #                         n.sampled = 0, 
+  #                         prop.sampled = 0)
+  #   newdat2$zday <- (newdat2$yday - 150) / 100
+  #   newdat2$prop.flooded <- predict(res$gam, newdat2, type = 'response')
+  #   pred2 <- as.data.frame(predict(res$gam, newdat2, type = 'link', se.fit = T))
+  #   newdat2$lcl <- plogis(pred2$fit - 2 * pred2$se.fit)
+  #   newdat2$ucl <- plogis(pred2$fit + 2 * pred2$se.fit)
+  #   
+  #   if (plot == TRUE) {
+  #     require(ggplot2)
+  #     p = ggplot(newdat, aes(x=yday, y=prop.flooded, ymin=lcl, ymax=ucl)) + 
+  #       geom_ribbon(fill='gray80') + geom_line() + 
+  #       geom_line(data=newdat2, aes(x=yday, y=prop.flooded), linetype='dashed') +
+  #       geom_ribbon(data=newdat2, aes(x=yday, y=prop.flooded), fill='gray90', alpha=0.5) +
+  #       theme_classic() + xlab(NULL) + ylab('Proportion open water') + 
+  #       geom_point(data=sub, aes(ymin=NULL, ymax=NULL, size=precip.2wk, color=precip.2wk)) + 
+  #       scale_x_continuous(limits=c(1,319), expand=c(0,0), breaks=c(1,32,62,93,124,154,185,216,244,275,305), 
+  #                          labels=c('Jul','Aug','Sep','Oct','Nov','Dec','Jan','Feb','Mar','Apr','May'))
+  #     print(p)      
+  #   }
+  #   res <- list(mod = res, sampledat = sampledat, pred = newdat)
+  #   return(res)
+  # }
+}
