040b04184fc451785c70a5589ef3fc863f815014
Kristen Dybala <kdybala@gmail.com>
2020-06-16 17:47:32 -0700

    add drake files for managing workflow

---
+++ packages.R
@@ -0,0 +1,20 @@
+## library() calls go here
+library(conflicted)
+library(dotenv)
+library(drake)
+library(tidyverse)
+library(sf)
+library(readxl)
+library(rnass) #remotes::install_github('emraher/rnass')
+library(gamm4)
+library(bioenergmod) #remotes::install_github('kdybala/bioenergmod')
+
+conflicted::conflict_prefer("filter", "dplyr")
+conflicted::conflict_prefer("select", "dplyr")
+conflicted::conflict_prefer("lag", "dplyr")
+conflicted::conflict_prefer("gather", "tidyr")
+
+# API for rnass package:
+# acquire from: https://quickstats.nass.usda.gov/api
+# --> assume stored as NASS_TOKEN in .Renviron
+# usethis::edit_r_environ()
